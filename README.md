bd69abb7f9c92247845b1817b6d284382cc2d37c
auro.mohanty <aurogreenindia@gmail.com>
2025-05-06 09:04:07 +0200

    feat: build and run calc.c

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # basic-calculator
 - Build a basic calculator in C.
 - Migrate a part of the C code into Rust.
+
+## To compile
+`gcc -o calc calc.c`
+
+## To run
+e.g. `./calc 2 + 3`
+
---
+++ calc.c
@@ -0,0 +1,38 @@
+// calc.c
+// Write a basic C CLI tool: calc.c - it just adds or subtracts two integers.
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s <num1> <operator> <num2>\n", argv[0]);
+        return 1;
+    }
+
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[3]);
+    char operator = argv[2][0];
+
+    switch (operator) {
+        case '+':
+            printf("%d\n", add(num1, num2));
+            break;
+        case '-':
+            printf("%d\n", subtract(num1, num2));
+            break;
+        default:
+            printf("Invalid operator. Use + or -.\n");
+            return 1;
+    }
+
+    return 0;
+}
